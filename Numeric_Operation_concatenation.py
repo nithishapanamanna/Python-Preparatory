89e3f9d16caee41c38dc45f1d1cd56ba98ebb266
nithishapanamanna <47380740+nithishapanamanna@users.noreply.github.com>
2025-10-13 19:38:24 +0530

    Add files via upload

---
+++ Numeric_Operation_concatenation.py
@@ -0,0 +1,16 @@
+#1. Numeric operations
+
+A = int( input( "Enter 1st number = " ))
+B = int( input( "Enter 2nd number = " ))
+print( "The sum of entered 2 number is = ", A+B )
+print( "The difference of entered 2 number is = ", A-B )
+print( "The product of entered 2 number is = ", A*B )
+print( "The result of division of entered 2 number is = ", A/B )
+
+#2. String Concatenation
+A = str(A)
+B = str(B)
+# or
+# A = str( input( "Enter the string = "))
+# B = str( input( "Enter the string = "))
+print( "The concatenated string is as = ", A+B )
